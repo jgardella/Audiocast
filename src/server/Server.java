d025bef62b0f4ef68f4ef26b3227901974cdb891
Jason Gardella <jgardell@stevens.edu>
2014-01-03 12:33:06 -0500

    Added ability to play source on serverside.

--- src/server/Server.java
+++ src/server/Server.java
@@ -11,6 +11,12 @@ import java.net.ServerSocket;
 import java.util.ArrayList;
 
 import javax.media.cdm.CaptureDeviceManager;
+import javax.media.CannotRealizeException;
+import javax.media.CaptureDeviceInfo;
+import javax.media.Manager;
+import javax.media.MediaLocator;
+import javax.media.NoPlayerException;
+import javax.media.Player;
 import javax.media.format.AudioFormat;
 import javax.swing.*;
 
@@ -25,44 +31,64 @@ public class Server implements ActionListener
 	private JComboBox<Source> sourceList;
 	private JTextArea area, availableSourcesArea;
 	private JLabel usersLabel;
-	private JButton addSource, removeSource, renameSource;
+	private JButton addSource, removeSource, renameSource, playSource;
 	private ArrayList<Source> availableSources;
+	private Player audioPlayer;
 	
 	public Server()
 	{
 		frame = new JFrame("Pierce Audiocast");
 		frame.setPreferredSize(new Dimension(235, 500));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		panel = new JPanel();
 		panel.setPreferredSize(new Dimension(235, 500));
+		
 		area = new JTextArea();
 		area.setEditable(false);
 		area.setPreferredSize(new Dimension(175, 175));
+		
 		sp = new JScrollPane(area);
+		
 		usersLabel = new JLabel("Users");
+		
 		panel.add(usersLabel);
 		panel.add(sp);
 		panel.add(new JLabel("Sources"));
+		
 		sourceList = new JComboBox<Source>();
 		for(int i = 1; i < CaptureDeviceManager.getDeviceList(new AudioFormat(AudioFormat.LINEAR)).size() + 1; i++)
 			sourceList.addItem(new Source("Source "+i, i));
+		
 		panel.add(sourceList);
+		
 		addSource = new JButton("Activate Source");
 		addSource.addActionListener(this);
 		addSource.setActionCommand("add");
+		
 		removeSource = new JButton("Deactivate Source");
 		removeSource.addActionListener(this);
 		removeSource.setActionCommand("remove");
+		
 		renameSource = new JButton("Rename Source");
 		renameSource.addActionListener(this);
 		renameSource.setActionCommand("rename");
+		
+		playSource = new JButton("Play Source");
+		playSource.addActionListener(this);
+		playSource.setActionCommand("play");
+		
 		panel.add(addSource);
 		panel.add(removeSource);
 		panel.add(renameSource);
+		panel.add(playSource);
+		
 		availableSourcesArea = new JTextArea();
 		availableSourcesArea.setPreferredSize(new Dimension(150, 125));
+		
 		panel.add(new JLabel("Active Sources"));
 		panel.add(availableSourcesArea);
+		
 		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
@@ -117,6 +143,9 @@ public class Server implements ActionListener
 		}
 	}
 	
+	/**
+	 * Refreshes the text in available sources and sends a source update datagram to through each serverthread.
+	 */
 	private void refreshAvailableSourceArea()
 	{
 		availableSourcesArea.setText("");
@@ -131,6 +160,7 @@ public class Server implements ActionListener
 	{
 		switch(e.getActionCommand())
 		{
+		// Adds the selected source to the available sources list.
 		case "add":
 			Source addSource = (Source)sourceList.getSelectedItem();
 			for(Source s : availableSources)
@@ -139,6 +169,7 @@ public class Server implements ActionListener
 			availableSources.add(addSource);
 			refreshAvailableSourceArea();
 			break;
+		// Removes the selected source from the available sources list.
 		case "remove":
 			Source removeSource = (Source)sourceList.getSelectedItem();
 			for(int i = 0; i < availableSources.size(); i++)
@@ -146,6 +177,7 @@ public class Server implements ActionListener
 					availableSources.remove(i);
 			refreshAvailableSourceArea();
 			break;
+		// Renames the selected source to another name.
 		case "rename":
 			String newName = JOptionPane.showInputDialog(frame, "New name:");
 			if(newName == null || newName.equals(""))
@@ -157,9 +189,35 @@ public class Server implements ActionListener
 			sourceList.repaint();
 			refreshAvailableSourceArea();
 			break;
+		// Plays the selected source, or stops it if it is already playing.
+		case "play":
+			try
+			{
+				if(playSource.getText() == "Play Source")
+				{
+					int index = ((Source)sourceList.getSelectedItem()).getIndex();
+					MediaLocator source = ((CaptureDeviceInfo)CaptureDeviceManager.getDeviceList(new AudioFormat(AudioFormat.LINEAR)).get(index-1)).getLocator();
+					audioPlayer = Manager.createRealizedPlayer(source);
+					audioPlayer.start();
+					playSource.setText("Stop Playing");
+				}
+				else
+				{
+					audioPlayer.stop();
+					audioPlayer.close();
+					playSource.setText("Play Source");
+				}
+			} catch (NoPlayerException | CannotRealizeException | IOException e1)
+			{
+				e1.printStackTrace();
+			}
+			break;
 		}
 	}
 	
+	/**
+	 * @return Returns the available sources list.
+	 */
 	public ArrayList<Source> getAvailableSources()
 	{
 		return availableSources;
